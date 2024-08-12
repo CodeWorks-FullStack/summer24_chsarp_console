afc32e58d6e2f2892e5d9ddad52c8d8c47689661
Jeremy Fowler <jembyfowler@gmail.com>
2024-08-12 10:32:07 -0600

    strings

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string firstName = "Jeremy";
+
+string lastName = "Fowler";
+
+char middleInitial = 'B';
+
+Console.WriteLine(firstName + middleInitial + lastName);
+
+Console.WriteLine($"My name is {firstName} {middleInitial} {lastName}!");
